a134e4926a40d1427e22f29df330eaa386f083ca
YeFei <316606233@qq.com>
2021-12-28 13:24:24 +0800

    utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
 /**
- * @param {object} obj
- * @param {string[]} path
- * @param {*} [value]
+ * 给指定对象设置新的属性，如果属性结构不存在则创建
+ * 
+ * @param obj 需要处理的对象
+ * @param path 对象属性
+ * @param value 是否需要赋值，如果不指定则为取值
  */
-export function propertyAt(obj, path, value) {
+export function propertyAt(obj: { [key: number | string | symbol]: any }, path: string[], value?: any) {
   let ref = obj;
   for (const p of path.slice(0, -1)) {
     if (ref[p] === undefined) {
