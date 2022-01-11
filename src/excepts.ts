2fd2d88ff4e090abf7a2fe145bf06f6c975cdb6c
YeFei <316606233@qq.com>
2022-01-11 10:39:59 +0800

    ts: excepts

--- src/excepts.ts
+++ src/excepts.ts
@@ -1,13 +1,5 @@
 export class ExtendableError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    if (typeof Error.captureStackTrace === 'function') {
-      Error.captureStackTrace(this, this.constructor);
-    } else { 
-      this.stack = (new Error(message)).stack; 
-    }
-  }
+  name = this.constructor.name;
 }
 
 export class DoesNotExist extends ExtendableError {}
